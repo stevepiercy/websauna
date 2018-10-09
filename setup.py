0bc3338a5da92d5d89c4664d9c9ee27f710fecc1
Érico Andrei <ericof@gmail.com>
2018-10-09 10:36:12 +0200

    Use newer version of pgcli

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ setup(
             "pyramid_notebook>=0.3.0",
         ],
         # Command line utilities and like that are needed to make development / production environment friendly
-        'utils': ['pgcli<2'],  # pgcli 2.0.0 is not compatible with ipython 5.1.0, because they have conflicting version restrictions on prompt-toolkit
+        'utils': ['pgcli>=2'],
         # Using celery based async tasks
         'celery': ['celery[redis]>=4.2.0,<5.0.0']
     },
